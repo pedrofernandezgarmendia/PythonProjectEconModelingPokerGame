9ebcb706dd226646ffa74284c8413b09c23ea8ea
pedrofernandez <pedrofernandezgarmendia@gmail.com>
2025-04-29 19:01:41 +0200

    Pokergame

--- deck.py
+++ deck.py
@@ -1,29 +1,30 @@
 import random
 
-class Card:
+class Card: # template to create objects
     RANKS = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"]
     SUITS = ["clubs", "diamonds", "hearts", "spades"]
-    def __init__(self, suit, rank):
+    def __init__(self, suit, rank): #the constructor, automatically called when you create a new card
         if rank not in self.RANKS:
-            raise ValueError("Invalid rank")
+            raise ValueError("Invalid rank") #validation
         if suit not in self.SUITS:
-            raise ValueError("Invalid suit")
-        self._suit = suit
-        self._rank = rank
+            raise ValueError("Invalid suit") #validation
+        self._suit = suit # internal use
+        self._rank = rank # internal use
 
     def __eq__(self, other):
-        return self.rank == other.rank
+        return self.rank == other.rank # two cards are equal if they have the same rank
 
     def __gt__(self, other):
-        return self.RANKS.index(self.rank) > self.RANKS.index(other.rank)
+        return self.RANKS.index(self.rank) > self.RANKS.index(other.rank) # checks the ranks and compares the indexes
+
 
     def __str__(self):
-        return f"{self._rank}{self._suit}"
+        return f"{self._rank}{self._suit}" # combines rank and suit into the same string (Qhearts)
 
     def __repr__(self):
-        return self.__str__()
+        return self.__str__() # reuses the __str__ method to keep it simple
 
-    @property
+    @property # hides internal variables and keeps the possibility open for future validation
     def suit(self):
         return self._suit
 
@@ -39,17 +40,17 @@ class Deck:
                 self._deck.append(Card(suit, rank))
 
     def __str__(self):
-        return str(self._deck)
+        return str(self._deck) # defines how the deck will be printed
 
     def shuffle(self):
-        random.shuffle(self._deck)
+        random.shuffle(self._deck) # randomizes order of cards
 
     def deal(self):
-        return self._deck.pop(0)
+        return self._deck.pop(0) # removes and returns the top cards
 
 if __name__ == "__main__":
-    deck = Deck()
-    print(deck)
-    deck.shuffle()
-    print(deck)
-    print(deck.deal())
+    deck = Deck() # creates a new deck
+    print(deck) # prints ordered deck
+    deck.shuffle() # shuffles the deck
+    print(deck) # prints the deck again (now shuffled)
+    print(deck.deal()) # deal removes first card and prints it
--- pokergame.py
+++ pokergame.py
@@ -16,18 +16,18 @@ class PokerHand:
         return str(self.cards)
 
     @property
-    def is_flush(self):
+    def is_flush(self): # checks if all cards have the same suit
         for card in self.cards[1:]:
-            if self.cards[0].suit != card.suit:
-                return False
-        return True
+            if self.cards[0].suit != card.suit: # checks the suit and compares it
+                return False # if one card is different it's false
+        return True # if its a flush
 
     @property
     def is_full_house(self):
-        return self.number_matches == 8
+        return self.number_matches == 8 # it checks if the number of matching ranks is exactly 8
 
     @property
-    def number_matches(self):
+    def number_matches(self): # counts how many matches exist between card ranks in the hand
         matches = 0
         for i in range(len(self.cards)):
             for j in range(len(self.cards)):
@@ -39,42 +39,42 @@ class PokerHand:
 
     @property
     def is_pair(self):
-        if self.number_matches == 2: # simple
+        if self.number_matches == 2: # one pair found
             return True
         return False
 
     @property
     def is_two_pair(self):
-        return self.number_matches == 4 # more advanced
+        return self.number_matches == 4 # two pairs (more advanced)
 
     @property
     def is_trips(self):
-        if self.number_matches == 6:
+        if self.number_matches == 6: # three cards of the same rank
             return True
         return False
 
     @property
     def is_quads(self):
-        if self.number_matches == 12:
+        if self.number_matches == 12: # four cards share the same rank
             return True
         return False
 
     @property
-    def is_straight(self):
+    def is_straight(self): # calculates the distance between the highest and lowest card ranks
         self.cards.sort()
         distance = Card.RANKS.index(self.cards[4].rank) - \
                    Card.RANKS.index(self.cards[0].rank)
         return self.number_matches == 0 and distance == 4
 
-count = 0
-matches = 0
-while matches < 10:
-    deck = Deck()
-    deck.shuffle()
-    hand = PokerHand(deck)
+count = 0 # tracks the total number of hands generated
+matches = 0 # tracks the number of hands that are straights
+while matches < 10: # loop keeps running until it finds 10 straights
+    deck = Deck() # new deck
+    deck.shuffle() # shuffles the deck
+    hand = PokerHand(deck) # deals a poker hand
     if hand.is_straight:
-        matches += 1
+        matches += 1 # increase matches by one
         print(hand)
-    count += 1
+    count += 1 # after every hand (straight ot noy) increase count by one
 
-print(f"probability of a full straight is {100*matches/count}%")
+print(f"probability of a full straight is {100*matches/count}%") # how often a straight occurred during the simulation
